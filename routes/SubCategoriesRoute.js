bb6adf40ce6d4ce337395fb750a6923a7600c1a4
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-01-07 16:38:10 +0530

    add change

--- routes/SubCategoriesRoute.js
+++ routes/SubCategoriesRoute.js
@@ -9,9 +9,11 @@ const {
     getSubCategoryByCategoryId,
 } = require('../controllers/subCategoriesControllers');
 
+const {uploadToS3}=require('../common/multerConfig');
+
 // Routes for CRUD operations
 router.get('/categoryid/:category_id', getSubCategoryByCategoryId);
-router.post('/', createSubCategories);
+router.post('/',uploadToS3, createSubCategories);
 router.get('/', getAllSubCategoriess);
 router.get('/:id', getSubCategoriesById);
 router.put('/:id', updateSubCategories);

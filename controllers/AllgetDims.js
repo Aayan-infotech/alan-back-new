312e2d48cf873eea0ecfe702df9fc1527f05ed2c
Aayaninfotech <Aayaninfotech>
2025-01-14 16:10:14 +0530

    user magement

--- controllers/AllgetDims.js
+++ controllers/AllgetDims.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const dimsModels = {
+    whiteHeight:  require('../models/dimsWHmodel'),
     Grid: require('../models/dimsGridModel'),
     Fin: require('../models/dimsFinModel'),
     Color: require('../models/dimsColorModel'),

9ca099f75cd7f67ba5affdcf4c3e5d1c78cef222
motract <abinashaayan>
2025-02-07 18:29:25 +0530

    addcorsurl

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const connectDB = require("./config/db");
 const uploadDir = require("path").join(__dirname, "uploads");
 const fs = require("fs");
 const session = require("express-session");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 
 dotenv.config();
 
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
+  fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const app = express();
@@ -19,84 +19,87 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json()); 
-app.use(fileUpload());  
-app.use('/uploads', express.static('uploads'));
+app.use(express.json());
+app.use(fileUpload());
+app.use("/uploads", express.static("uploads"));
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://44.196.64.110:2040"],
+    methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
 app.use(bodyParser.json());
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: false },
-    })
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
 );
 
 connectDB();
 
 const AppointmentRoutes = require("./routes/appointmentRoute");
-const adminUserManageRoutes = require("./routes/adminUserManageRoutes")
-const categoryRoutes  = require("./routes/categoriesRoute")
-const subcategoryRoutes  = require("./routes/SubCategoriesRoute")
-const subSubCategories  = require("./routes/subSubCategoriesRoute")
-const productRoutes = require("./routes/ProductRoutes")
-const dimsGridRoutes = require("./routes/dimsGridRoute")
-const dimsFinRoutes = require("./routes/dimsFinRoute")
-const dimsColorRoutes = require("./routes/dimsColorRoute")
-const dimsTamperRoute = require("./routes/dimsTamperRoute")
-const dimsInstRoute = require("./routes/dimsInstRoute")
-const dimsLockRoute = require("./routes/dimsLockRoute")
-const dimsGTypeRoute = require("./routes/dimsGTypeRoute")
-const dimsPSpacingRoute = require("./routes/dimsPSpacingRoute")
-const dimsSWinOpensRoute = require("./routes/dimsSWinOpensRoute")
-const dimsWHroutes = require("./routes/dimsWHroutes")
-const AllgetDimsRoutes = require("./routes/AllgetDimsRoutes")
-const ProductImgRoutes = require("./routes/ProductImgRoutes")
-const CustMngRoutes = require("./routes/CustMngRoutes")
-const productFormula=require('./routes/prodcutFormulaRoutes')
-const orderRoutes=require('./routes/orderRoutes')
-const transactionRoutes=require('./routes/transactionRoutes')
-const WishlistRoutes=require('./routes/WishlistRoutes')
-const GMCardsRoutes=require('./routes/GMCardsRoutes')
-const paymentRoutes = require('./routes/paymentRoutes');
-const getAllCustData = require('./routes/FinalOrderRoutes');
+const adminUserManageRoutes = require("./routes/adminUserManageRoutes");
+const categoryRoutes = require("./routes/categoriesRoute");
+const subcategoryRoutes = require("./routes/SubCategoriesRoute");
+const subSubCategories = require("./routes/subSubCategoriesRoute");
+const productRoutes = require("./routes/ProductRoutes");
+const dimsGridRoutes = require("./routes/dimsGridRoute");
+const dimsFinRoutes = require("./routes/dimsFinRoute");
+const dimsColorRoutes = require("./routes/dimsColorRoute");
+const dimsTamperRoute = require("./routes/dimsTamperRoute");
+const dimsInstRoute = require("./routes/dimsInstRoute");
+const dimsLockRoute = require("./routes/dimsLockRoute");
+const dimsGTypeRoute = require("./routes/dimsGTypeRoute");
+const dimsPSpacingRoute = require("./routes/dimsPSpacingRoute");
+const dimsSWinOpensRoute = require("./routes/dimsSWinOpensRoute");
+const dimsWHroutes = require("./routes/dimsWHroutes");
+const AllgetDimsRoutes = require("./routes/AllgetDimsRoutes");
+const ProductImgRoutes = require("./routes/ProductImgRoutes");
+const CustMngRoutes = require("./routes/CustMngRoutes");
+const productFormula = require("./routes/prodcutFormulaRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const WishlistRoutes = require("./routes/WishlistRoutes");
+const GMCardsRoutes = require("./routes/GMCardsRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const getAllCustData = require("./routes/FinalOrderRoutes");
 
-
-
-app.use('/api', adminUserManageRoutes);
+app.use("/api", adminUserManageRoutes);
 app.use("/api/appointments", AppointmentRoutes);
-app.use('/api/categories', categoryRoutes );
-app.use('/api/subcategory', subcategoryRoutes );
-app.use('/api/subSubCategories', subSubCategories );
-app.use('/api/Products', productRoutes);
-app.use('/api/dimsGrid', dimsGridRoutes);
-app.use('/api/dimsFin', dimsFinRoutes);
-app.use('/api/dimsColor', dimsColorRoutes);
-app.use('/api/dimsTamper', dimsTamperRoute);
-app.use('/api/dimsInst', dimsInstRoute);
-app.use('/api/dimsLock', dimsLockRoute);
-app.use('/api/dimsGType', dimsGTypeRoute);
-app.use('/api/dimsPSpacing', dimsPSpacingRoute);
-app.use('/api/dimsSWinOpens', dimsSWinOpensRoute);
-app.use('/api/dimsWH', dimsWHroutes);
-app.use('/api/dims', AllgetDimsRoutes);
-app.use('/api/ProductImg', ProductImgRoutes);
-app.use('/api/CustMng', CustMngRoutes);
-app.use('/api/prodFormula',productFormula)
-app.use('/api/order',orderRoutes );
-app.use('/api/transaction',transactionRoutes)
-app.use('/api/wishlist',WishlistRoutes)
-app.use('/api/GMCards',GMCardsRoutes)
-app.use('/api/payment', paymentRoutes);
-app.use('/api/FnalCustData', getAllCustData);
-
-
-           
-
+app.use("/api/categories", categoryRoutes);
+app.use("/api/subcategory", subcategoryRoutes);
+app.use("/api/subSubCategories", subSubCategories);
+app.use("/api/Products", productRoutes);
+app.use("/api/dimsGrid", dimsGridRoutes);
+app.use("/api/dimsFin", dimsFinRoutes);
+app.use("/api/dimsColor", dimsColorRoutes);
+app.use("/api/dimsTamper", dimsTamperRoute);
+app.use("/api/dimsInst", dimsInstRoute);
+app.use("/api/dimsLock", dimsLockRoute);
+app.use("/api/dimsGType", dimsGTypeRoute);
+app.use("/api/dimsPSpacing", dimsPSpacingRoute);
+app.use("/api/dimsSWinOpens", dimsSWinOpensRoute);
+app.use("/api/dimsWH", dimsWHroutes);
+app.use("/api/dims", AllgetDimsRoutes);
+app.use("/api/ProductImg", ProductImgRoutes);
+app.use("/api/CustMng", CustMngRoutes);
+app.use("/api/prodFormula", productFormula);
+app.use("/api/order", orderRoutes);
+app.use("/api/transaction", transactionRoutes);
+app.use("/api/wishlist", WishlistRoutes);
+app.use("/api/GMCards", GMCardsRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/FnalCustData", getAllCustData);
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });

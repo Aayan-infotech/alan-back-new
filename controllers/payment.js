b7f8034d9e70e5ca9476d3e7af919d035c98a311
Aayaninfotech <Aayaninfotech>
2025-02-17 16:36:55 +0530

    api door

--- controllers/payment.js
+++ controllers/payment.js
@@ -4,6 +4,65 @@ const FinalOrder = require("../models/FinalOrderModel");
 const CustomerManage = require("../models/CustMngModel");
 const Order = require("../models/OrderModel");
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const CryptoJS = require("crypto-js");
+
+// exports.createPaymentIntent = async (req, res) => {
+//   try {
+//     const { checkoutData } = req.body;
+//     if (
+//       !checkoutData ||
+//       !checkoutData.totalPrice ||
+//       !checkoutData.totalProducts ||
+//       !checkoutData.shippingMethod
+//     ) {
+//       return res.status(400).json({ error: "Invalid checkout data." });
+//     }
+//     const totalPriceInPaise = Math.round(
+//       parseFloat(checkoutData.totalPrice) * 100
+//     );
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       line_items: [
+//         {
+//           price_data: {
+//             currency: "inr",
+//             product_data: {
+//               name: "Total Purchase",
+//               description: `Total Products: ${checkoutData.totalProducts}`,
+//             },
+//             unit_amount: totalPriceInPaise,
+//           },
+//           quantity: 1,
+//         },
+//       ],
+//       mode: "payment",
+//       // success_url: "http://44.196.64.110:2040//successfull?session_id={CHECKOUT_SESSION_ID}",
+//       // cancel_url: "http://44.196.64.110:2040//cancel",
+//       success_url: `${process.env.ROUTE_BASE_URL}/successfull?session_id={CHECKOUT_SESSION_ID}`,
+//       cancel_url: `${process.env.ROUTE_BASE_URL}/cancel`,
+//       metadata: {
+//         userId: checkoutData.userId,
+//         order_id: checkoutData.order_id,
+//         totalPrice: checkoutData.totalPrice,
+//         totalProducts: checkoutData.totalProducts,
+//       },
+//     });
+//     const transaction = new Transaction({
+//       userId: checkoutData.userId,
+//       order_id: checkoutData.order_id,
+//       amount: session.amount_total / 100,
+//       quantity: parseInt(session.metadata.totalProducts),
+//       currency: session.currency,
+//       paymentId: session.id,
+//       status: session.payment_status,
+//     });
+//     await transaction.save();
+//     res.json({ sessionId: session.id, session: session });
+//   } catch (error) {
+//     console.error("Payment Processing Error:", error.message);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
 exports.createPaymentIntent = async (req, res) => {
   try {
@@ -55,8 +114,19 @@ exports.createPaymentIntent = async (req, res) => {
       paymentId: session.id,
       status: session.payment_status,
     });
+
     await transaction.save();
-    res.json({ sessionId: session.id, session: session });
+
+    const encryptedSecret = CryptoJS.AES.encrypt(
+      String(process.env.STRIPE_SECRET_KEY),
+      String(process.env.ENCRYPTION_KEY)
+    ).toString();
+
+    res.json({
+      sessionId: session.id,
+      session: session,
+      client_secret: encryptedSecret,
+    });
   } catch (error) {
     console.error("Payment Processing Error:", error.message);
     res.status(500).json({ error: error.message });

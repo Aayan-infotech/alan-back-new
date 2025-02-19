02b0a9664bf77d31331d3ff200817d9b5431b596
Aayaninfotech <Aayaninfotech>
2025-02-19 15:31:54 +0530

    api door

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -42,6 +42,59 @@ const Product = require('../models/ProductModel');
 
 
 
+// exports.searchByName = async (req, res) => {
+//     try {
+//         const { name } = req.query;
+
+//         if (!name) {
+//             return res.status(400).json({
+//                 status: 400,
+//                 success: false,
+//                 message: "Name query parameter is required",
+//                 data: [],
+//             });
+//         }
+
+//         const query = { name: { $regex: name, $options: 'i' } };
+//         const [subCategories, subSubCategories, products] = await Promise.all([
+//             SubCategory.find(query),
+//             subSubCategory.find(query),
+//             Product.find(query),
+//         ]);
+
+//         // Check if all are empty
+//         if (!subCategories.length && !subSubCategories.length && !products.length) {
+//             return res.status(200).json({
+//                 status: 200,
+//                 success: true,
+//                 message: "No data available for this search",
+//                 data: [],
+//             });
+//         }
+
+//         // Add `isSubSubCategory` key
+//         const isSubSubCategory = subSubCategories.length > 0;
+//         subCategories.push.isSubSubCategory
+
+//         // Return search results
+//         res.status(200).json({
+//             status: 200,
+//             success: true,
+//             message: "Search results retrieved successfully",
+//             data: { subCategories, subSubCategories, products, isSubSubCategory },
+//         });
+
+//     } catch (error) {
+//         res.status(500).json({
+//             status: 500,
+//             success: false,
+//             message: "Internal server error",
+//             error: error.message,
+//         });
+//     }
+// };
+
+
 exports.searchByName = async (req, res) => {
     try {
         const { name } = req.query;
@@ -72,12 +125,20 @@ exports.searchByName = async (req, res) => {
             });
         }
 
+        // Add `isSubSubCategory` key in each `subCategory`
+        const updatedSubCategories = subCategories.map(subCategory => {
+            const hasSubSubCategory = subSubCategories.some(subSub => 
+                subSub.sub_category_id.toString() === subCategory._id.toString()
+            );
+            return { ...subCategory.toObject(), isSubSubCategory: hasSubSubCategory };
+        });
+
         // Return search results
         res.status(200).json({
             status: 200,
             success: true,
             message: "Search results retrieved successfully",
-            data: { subCategories, subSubCategories, products },
+            data: { subCategories: updatedSubCategories, subSubCategories, products },
         });
 
     } catch (error) {

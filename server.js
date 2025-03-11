091cf067950ba39fc8f4639667151889503ec6e1
Aayaninfotech <Aayaninfotech>
2025-03-11 11:03:34 +0530

    add html page in server side

--- server.js
+++ server.js
@@ -45,8 +45,9 @@ app.use(
 );
 
 // show page
-app.use(express.static("public"));
 const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
